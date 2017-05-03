d0fd4b21f275dc723ea9b08d04f73befe1a58167
justin <jneezy2008@gmail.com>
2017-05-03 16:47:14 -0400

    Initial commit Added via Pycharm

---
+++ Hangman_Messing_Around.py
@@ -0,0 +1,43 @@
+import random
+
+word_with_dashes = ''
+lives_remaining = 14
+
+
+if lives_remaining == 0:
+    print('You are out of lives.')
+else:
+    words_list = ['cat', 'mouse', 'dog', 'snake', 'pig', 'horse']
+    random_word = random.choice(words_list)
+    print('Random word is: ', random_word)
+
+    user_guess = input('Guess a letter: ')
+    print('You guessed ' + user_guess)
+    
+# For each letter in random_word, if user_guess is in random_word, add the letter
+# to a variable to be printed later
+
+    for x in random_word:
+        if x == user_guess:
+            word_with_dashes = word_with_dashes + user_guess
+        else:
+            word_with_dashes = word_with_dashes + '-'
+        continue
+
+print(word_with_dashes)
+
+
+'''
+### PSEUDO CODE ###
+
+First,  check if the user has more than 14 lives. If True, randomly select a word
+from words_list. If False, game ends.
+
+If the user has more than 14 lives remaining, ask the user to guess one of
+the letters in random_word. If they guess a letter correctly then update word_with_dashes
+with the user_guess. If they guess incorrectly, update word_with_dashes with a '-'.
+
+Ask the user for another guess then evaluate until all letters have been guessed.
+
+
+'''
